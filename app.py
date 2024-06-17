5aa93cce56b0d9f2e9205a1b0757f6cbf8e2db72
Abhishek Verma <43588391+Abhishek-Verma99@users.noreply.github.com>
2024-06-17 23:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Welcome to Python Flask Application - Building a flask app using AWS Codepipeline, AWS codebiuld and Codedeploy"
+def home():
+    return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
